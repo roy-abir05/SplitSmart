f61408b6f3fd8de25df7c1bd0966d18858ca5772
Chayan_Mallick_01 <chayanmallick2003@gmail.com>
2025-06-12 22:26:04 +0530

    Server initialisation

---
+++ Server/index.js
@@ -0,0 +1,47 @@
+// imports
+require('dotenv').config();
+
+
+const express = require('express');
+const app = express();
+
+// Middlewares
+
+// --> for json
+app.use(express.json());
+
+// for parsing the cookies
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+// for file parsing
+const fileUpload = require('express-fileupload');
+app.use(
+    fileUpload(
+        {
+            useTempFiles:true, // have a temporary path for the file
+            tempFileDir:'/tmp', // path of local directory
+        }
+    )
+)
+
+//cors configaration
+const cors = require('cors');
+app.use(
+    cors(
+        {
+            origin:`${process.env.FRONT_END_BASE_URL}`,
+            credentials:true,
+        }
+    )
+)
+
+//get the port number
+const PORT = process.env.PORT || 4000;
+
+// make the app listen to incomming requests from client
+app.listen(PORT,() => {
+    console.log('The Backend Server is Listening at Port Number ',PORT);
+});
+
+// connect to postgress sql db
